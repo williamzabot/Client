8659845deca8e42d6ffdeccb7b4446efb3fcc4e5
williamzabot <williamzabot123@gmail.com>
2023-07-07 21:04:24 -0300

    improvements

--- Client.py
+++ Client.py
@@ -3,16 +3,25 @@ from socket import *
 # no servidor é utilizada a connection para enviar uma mensagem para o cliente
 # enquanto no cliente é utilizado o socketServer
 serverHost = "localhost"
-serverPort = 50001
+serverPort = 1024
 socketClient = socket(AF_INET, SOCK_STREAM)
 print('Cliente solicitando conexão com o servidor ...')
 socketClient.connect((serverHost, serverPort))
 print('Cliente conectado com o servidor ...')
+
+def receive():
+    return socketClient.recv(1024).decode("utf-8")
+
+def send(message):
+    socketClient.send(message.encode("utf-8"))
+
 while True:
-    msg = str(input("Digite uma mensagem: "))
-    socketClient.send(msg.encode("utf-8"))
     msgServidor = socketClient.recv(1024).decode("utf-8")
     print(msgServidor)
-    if msg == ":q" or msgServidor == ":q":
+    msg = str(input("Escolha: "))
+    socketClient.send(msg.encode("utf-8"))
+    if msgServidor == ":q":
         socketClient.close()
         break
+
+
